a7f86b28cb7a4ceb8a3e8d1bee3d0d6eb10a016c
KaysData <kay-ayala@protonmail.com>
2021-06-07 14:15:57 -0700

    added requirements.txt

--- flask-docker/flaskapp-docker/flask-app/app.py
+++ flask-docker/flaskapp-docker/flask-app/app.py
@@ -1,4 +1,4 @@
-app = flask import Flask, render_template, url_for, redirect 
+from flask import Flask, render_template, url_for, redirect 
 app = Flask(__name__)
 
 @app.route("/")
@@ -12,3 +12,6 @@ def home():
 @app.route("/login")
 def login():
     return render_template("login.html")
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
